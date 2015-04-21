145c9b9be6fa70a4b20e5e77a8be7c9489496c6c
garzarodrigo <rgarza2016@kellogg.northwestern.edu>
2015-04-21 07:08:19 -0500

    Class work

--- app/controllers/essentials_controller.rb
+++ app/controllers/essentials_controller.rb
@@ -11,6 +11,7 @@ class EssentialsController < ApplicationController
     full = first + " " + last
     lucky_number = rand(1..100) + 7
 
+
     # Adding strings together with the + operator
     @message1 = "Howdy, " + full + ". Your lucky number for today is " + lucky_number.to_s + "."
     # is harder than with String Interpolation
